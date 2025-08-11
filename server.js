aa1bd032e8b5e22cd9f21e51ff0e13e8a0b96b68
Phawaaz <phawaazakin@gmail.com>
2025-08-11 21:17:36 +0100

    initializing the db

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './.env' });
+// console.log(app.get('env'));
+// console.log(process.env); // This will log the current environment, e.g., 'development' or 'production'
+
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`running on port: ${port}.....`);
 });
