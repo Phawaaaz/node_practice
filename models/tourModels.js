3c9104b971a1fa0c684f25693636174903635d42
Phawaaz <phawaazakin@gmail.com>
2025-08-20 11:24:41 +0100

    Fix tour controller and routes

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,57 +1,84 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+````;
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+    },
+    slug: String,
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    priceDiscount: Number,
+    Summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a description'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    startDates: [Date],
   },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  priceDiscount: Number,
-  Summary: {
-    type: String,
-    trim: true,
-    // required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a description'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
-  startDates: [Date],
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
+// Document middleware: run before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } }); // Exclude secret tours
+  this.start = Date.now();
+  next();
+});
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,17 +3,20 @@ const tourController = require('../controllers/tourController');
 // const { getAllTours, getTour, newTour, updateTour, deleteTour } =
 //   tourController;
 
-
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours)
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
+router.route('/tour-stat').get(tourController.getTourStats);
+router.route('/Montly-plan/:year').get(tourController.getMonthlyPlan);
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post( tourController.createTour);
+  .post(tourController.createTour);
 router
   .route('/:id')
   .get(tourController.getTour)
